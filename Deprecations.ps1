119ec94212bd2a0290d77ebba28e30f34a98ce58
George Ollis <76496924+georgeollis@users.noreply.github.com>
2024-05-29 16:45:35 +0100

    Create Deprecations.ps1

---
+++ Deprecations.ps1
@@ -0,0 +1,43 @@
+
+function Get-AzureDeprections {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)][bool]$FutureOnly = $false
+    )
+    
+    try {
+        [xml]$xmldoc = (Invoke-WebRequest -Uri "https://aztty.azurewebsites.net/rss/deprecations").Content.Substring(1) # Get Azure deprecations
+    }
+    catch {
+        throw $_.Exception.Message
+    }
+    
+    
+    $list = [System.Collections.ArrayList]@() # Create an empty array
+
+    foreach ($item in $xmldoc.rss.channel.item) {
+
+        $date = (((($item.description).Split(",")[1]).Trim() | Select-String -Pattern "\d{1,2}/\d{1,2}/\d{4}").Matches.Value) # Regex find date pattern
+        $service = ($item.description).Split(",")[0].Split(":")[1].Trim() # Select the service name
+        $title = ($item.title).Trim() # Remove white space from title
+
+
+        $object = [PSCustomObject]@{
+            title          = $title
+            deprectionDate = [datetime]$date
+            link           = $item.link.href
+            service        = $service
+        }
+
+        $list.Add($object) | Out-Null # Add custom object to the array.
+        
+
+    }
+
+    if ($FutureOnly) {
+        $list = $list | Where-Object { $_.deprectionDate -gt (Get-Date) } 
+    }
+
+    return $list # return the list to the terminal
+    
+}
